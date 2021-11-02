92fa0d8511ea4aad3143271d43be8851898dcb46
Mark Andersen <10101087+MarkAndersen@users.noreply.github.com>
2021-11-02 13:01:57 -0400

    finished login route and redirects, stored sessions, password validation, etc.

--- app/models/User.py
+++ app/models/User.py
@@ -25,3 +25,10 @@ class User(Base):
 
         # encrypt password
         return bcrypt.hashpw(password.encode('utf-8'), salt)
+
+    # checks password entered to encrypted password stored
+    def verify_password(self, password):
+        return bcrypt.checkpw(
+            password.encode('utf-8'),
+            self.password.encode('utf-8')
+        )
