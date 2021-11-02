e4ab1bc077d894bbad34b54ad1fb8917b71aaf87
Mark Andersen <10101087+MarkAndersen@users.noreply.github.com>
2021-11-02 12:43:03 -0400

    added signup error handling, try-except block instead of try-catch like in JS

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify
 from app.models import User
 from app.db import get_db
+import sys
 
 bp = Blueprint('api', __name__, url_prefix='/api')
 
@@ -13,14 +14,23 @@ def signup():
    
      # use bracket notation instead of dot notation because USER is not an object in python, it is a 'library', it is only an object when creating class and attaching methods to it
      # this creates a new instance of a 'User'
-    newUser = User(
-        username = data['username'],
-        email = data['email'],
-        password = data['password']
-    )
+     # use a try .. except statement (like try-catch block in JS) for error handling, be very careful abount indentation
+    try:
+        newUser = User(
+            username = data['username'],
+            email = data['email'],
+            password = data['password']
+        )
 
     # save in db
-    db.add(newUser)
-    db.commit()
+        db.add(newUser)
+        db.commit()
+    except:
+        print(sys.exc_info()[0])
+
+        # insert failed, rollback and then send error to front end - helps prevent crashes with pending db connections
+        db.rollback()
+        return jsonify(message = 'signup failed'), 500
+
 
     return jsonify(id = newUser.id)
